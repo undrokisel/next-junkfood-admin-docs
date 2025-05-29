0cac980ee794ae427237d5a2eb72858bee83f708
kiselandrey <undrokisel@gmail.com>
2025-05-29 15:25:29 +0500

    final

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Твоя шаурма - документация админ-панели</span>,
+  footer: {
+    text: 'Создано А.В. Киселем, ОНИКС, 2025',
+  },
+  search: {
+    component: null
+  },
+  feedback: {content: null},
+  editLink: {component: null},
+  toc: {title: "На этой странице"},
+  gitTimestamp: ''
+}
+
+export default config
